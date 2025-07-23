af0f9d4dc64d0d1f64f3362e304c90f0ef91b310
YJE <c0d1ngyje@gmail.com>
2025-07-23 16:18:18 +0900

    feat: (#8) auth 컨트롤러 작성

--- app/src/routes/auth/auth.ctrl.js
+++ app/src/routes/auth/auth.ctrl.js
@@ -1,8 +1,38 @@
 "use strict";
 
+const AuthService = require("../../services/auth/authService.js");
+
 const process = {
-    signUp: (req, res) => {},
-    login: (req, res) => {},
-}
+  signUp: async (req, res) => {
+    try {
+      const authService = new AuthService(req);
+      const { statusCode, data } = await authService.signUp();
+      return res.status(statusCode).json(data);
+    } catch (error) {
+      console.error("signUp error: ", error);
+      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const authService = new AuthService(req);
+      const { statusCode, data } = await authService.login();
+      return res.status(statusCode).json(data);
+    } catch (error) {
+      console.error("login error: ", error);
+      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+    }
+  },
+  newAccessToken: async (req, res) => {
+    try {
+      const authService = new AuthService(req);
+      const { statusCode, data } = await authService.newAccessToken();
+      return res.status(statusCode).json(data);
+    } catch (error) {
+      console.error("newAccessToken error:", error);
+      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+    }
+  },
+};
 
 module.exports = { process };
