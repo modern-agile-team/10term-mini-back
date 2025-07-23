113923f9d6bc809580b17f97609d8137824365e4
YJE <c0d1ngyje@gmail.com>
2025-07-23 22:30:15 +0900

    refactor: (#8) 컨트롤러 응답 포맷 정리

--- app/src/routes/auth/auth.ctrl.js
+++ app/src/routes/auth/auth.ctrl.js
@@ -6,31 +6,40 @@ const process = {
   signUp: async (req, res) => {
     try {
       const authService = new AuthService(req);
-      const { statusCode, data } = await authService.signUp();
-      return res.status(statusCode).json(data);
+      const { status, success, data } = await authService.signUp();
+      return res.status(status).json({ success, data });
     } catch (error) {
       console.error("signUp error: ", error);
-      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+      return res.status(500).json({
+        success: false,
+        data: { message: "서버 오류가 발생했습니다." },
+      });
     }
   },
   login: async (req, res) => {
     try {
       const authService = new AuthService(req);
-      const { statusCode, data } = await authService.login();
-      return res.status(statusCode).json(data);
+      const { status, success, data } = await authService.login();
+      return res.status(status).json({ success, data });
     } catch (error) {
       console.error("login error: ", error);
-      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+      return res.status(500).json({
+        success: false,
+        data: { message: "서버 오류가 발생했습니다." },
+      });
     }
   },
   newAccessToken: async (req, res) => {
     try {
       const authService = new AuthService(req);
-      const { statusCode, data } = await authService.newAccessToken();
-      return res.status(statusCode).json(data);
+      const { status, success, data } = await authService.newAccessToken();
+      return res.status(status).json({ success, data });
     } catch (error) {
       console.error("newAccessToken error:", error);
-      return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+      return res.status(500).json({
+        success: false,
+        data: { message: "서버 오류가 발생했습니다." },
+      });
     }
   },
 };
