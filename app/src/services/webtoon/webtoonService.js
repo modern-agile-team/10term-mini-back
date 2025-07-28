71a948195d69cc6dc42619b5c884a74aebc96614
LDH <leedoh0384@gmail.com>
2025-07-28 22:33:23 +0900

    feat: (#16) 정렬조건에 따른 웹툰 목록 조회
    - like, updated, view, rate 기준에 따라 정렬 가능하도록 로직 구현

--- app/src/services/webtoon/webtoonService.js
+++ app/src/services/webtoon/webtoonService.js
@@ -9,20 +9,53 @@ class WebtoonService {
 
   async getWebtoons(filters) {
     try {
-      const { day } = filters;
-      if (!day) {
-        const allWebtoons = await this.webtoonRepository.getAllWebtoons();
+      const { day, sort } = filters;
+
+      const dbKey = {
+        like: "like_count",
+        updated: "updated_at",
+        view: "view_count",
+        rate: "rating_avg",
+      };
+
+      const dbSortKey = dbKey[sort];
+
+      if (sort && !dbSortKey) throw new Error("유효하지 않은 sort 값입니다.");
+
+      if (day) {
+        if (dbSortKey) {
+          const webtoonsByDaySorted = await this.webtoonRepository.getWebtoonsByDaySorted(
+            day,
+            dbSortKey
+          );
+          return {
+            status: 200,
+            success: true,
+            data: { webtoonsByDaySorted },
+          };
+        }
+        const webtoonsByDay = await this.webtoonRepository.getWebtoonsByDay(day);
         return {
           status: 200,
           success: true,
-          data: { allWebtoons },
+          data: { webtoonsByDay },
         };
       }
-      const webtoonsByDay = await this.webtoonRepository.getWebtoonsByDay(day);
+
+      if (dbSortKey) {
+        const allWebtoonsSorted = await this.webtoonRepository.getAllWebtoonsSorted(dbSortKey);
+        return {
+          status: 200,
+          success: true,
+          data: { allWebtoonsSorted },
+        };
+      }
+
+      const allWebtoons = await this.webtoonRepository.getAllWebtoons();
       return {
         status: 200,
         success: true,
-        data: { webtoonsByDay },
+        data: { allWebtoons },
       };
     } catch (error) {
       console.error("Error occurred while fetching webtoons:", error);
