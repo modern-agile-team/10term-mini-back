f5f77cc5af1e20368caf2a02fc246a55c57d8e15
YJE <c0d1ngyje@gmail.com>
2025-08-06 15:35:14 +0900

    refactor: (#25) controller에서 try-catch 블록 제거

--- app/src/routes/auth/authController.js
+++ app/src/routes/auth/authController.js
@@ -5,79 +5,63 @@ const { setRefreshToken, clearRefreshToken } = require("@utils/cookie.js");
 
 module.exports = {
   signUp: async (req, res, next) => {
-    try {
-      const { username, password, nickname } = req.body;
-      const authService = new AuthService();
-      const data = await authService.signUp(username, password, nickname);
+    const { username, password, nickname } = req.body;
+    const authService = new AuthService();
+    const data = await authService.signUp(username, password, nickname);
 
-      if (data.refreshToken) {
-        setRefreshToken(res, data.refreshToken);
-        delete data.refreshToken;
-      }
-
-      return res.status(201).json({
-        success: true,
-        data: {
-          message: "회원가입 성공",
-          content: data,
-        },
-      });
-    } catch (error) {
-      next(error);
+    if (data.refreshToken) {
+      setRefreshToken(res, data.refreshToken);
+      delete data.refreshToken;
     }
+
+    return res.status(201).json({
+      success: true,
+      data: {
+        message: "회원가입 성공",
+        content: data,
+      },
+    });
   },
 
   login: async (req, res, next) => {
-    try {
-      const { username, password } = req.body;
-      const authService = new AuthService();
-      const data = await authService.login(username, password);
+    const { username, password } = req.body;
+    const authService = new AuthService();
+    const data = await authService.login(username, password);
 
-      if (data.refreshToken) {
-        setRefreshToken(res, data.refreshToken);
-        delete data.refreshToken;
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: {
-          message: "로그인 성공",
-          content: data,
-        },
-      });
-    } catch (error) {
-      next(error);
+    if (data.refreshToken) {
+      setRefreshToken(res, data.refreshToken);
+      delete data.refreshToken;
     }
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        message: "로그인 성공",
+        content: data,
+      },
+    });
   },
 
   issueAccessToken: async (req, res, next) => {
-    try {
-      const refreshToken = req.cookies.refreshToken;
-      const authService = new AuthService();
-      const accessToken = await authService.issueAccessToken(refreshToken);
+    const refreshToken = req.cookies.refreshToken;
+    const authService = new AuthService();
+    const accessToken = await authService.issueAccessToken(refreshToken);
 
-      return res.status(200).json({
-        success: true,
-        data: {
-          message: "access 토큰 발급 성공",
-          content: accessToken,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        message: "access 토큰 발급 성공",
+        content: accessToken,
+      },
+    });
   },
 
   logout: async (req, res, next) => {
-    try {
-      clearRefreshToken(res);
+    clearRefreshToken(res);
 
-      return res.status(200).json({
-        success: true,
-        data: { message: "로그아웃 되었습니다." },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({
+      success: true,
+      data: { message: "로그아웃 되었습니다." },
+    });
   },
 };
--- app/src/routes/comment/commentController.js
+++ app/src/routes/comment/commentController.js
@@ -4,82 +4,66 @@ const CommentService = require("@services/comment/commentService.js");
 
 module.exports = {
   createComment: async (req, res, next) => {
-    try {
-      const { episodeId } = req.params;
-      const { content, parentId } = req.body;
-      const userId = req.user.id;
+    const { episodeId } = req.params;
+    const { content, parentId } = req.body;
+    const userId = req.user.id;
 
-      const commentService = new CommentService();
-      const newComment = await commentService.createComment(episodeId, content, parentId, userId);
+    const commentService = new CommentService();
+    const newComment = await commentService.createComment(episodeId, content, parentId, userId);
 
-      return res.status(201).json({
-        success: true,
-        data: {
-          message: "댓글 작성 성공",
-          content: newComment,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(201).json({
+      success: true,
+      data: {
+        message: "댓글 작성 성공",
+        content: newComment,
+      },
+    });
   },
 
   updateComment: async (req, res, next) => {
-    try {
-      const { commentId } = req.params;
-      const { content } = req.body;
-      const userId = req.user.id;
+    const { commentId } = req.params;
+    const { content } = req.body;
+    const userId = req.user.id;
 
-      const commentService = new CommentService();
-      const updatedComment = await commentService.updateComment(commentId, content, userId);
+    const commentService = new CommentService();
+    const updatedComment = await commentService.updateComment(commentId, content, userId);
 
-      return res.status(200).json({
-        success: true,
-        data: {
-          message: "댓글 수정 성공",
-          content: updatedComment,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        message: "댓글 수정 성공",
+        content: updatedComment,
+      },
+    });
   },
 
   deleteComment: async (req, res, next) => {
-    try {
-      const { commentId } = req.params;
-      const userId = req.user.id;
+    const { commentId } = req.params;
+    const userId = req.user.id;
 
-      const commentService = new CommentService();
-      await commentService.deleteComment(commentId, userId);
+    const commentService = new CommentService();
+    await commentService.deleteComment(commentId, userId);
 
-      return res.status(200).json({
-        success: true,
-        data: { message: "댓글 삭제 성공" },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({
+      success: true,
+      data: { message: "댓글 삭제 성공" },
+    });
   },
 
   reactComment: async (req, res, next) => {
-    try {
-      const { commentId } = req.params;
-      const { type } = req.body;
-      const userId = req.user.id;
+    const { commentId } = req.params;
+    const { type } = req.body;
+    const userId = req.user.id;
 
-      const commentService = new CommentService();
-      const reactionData = await commentService.reactComment(commentId, type, userId);
+    const commentService = new CommentService();
+    const reactionData = await commentService.reactComment(commentId, type, userId);
 
-      return res.status(200).json({
-        success: true,
-        data: {
-          message: "댓글 반응 처리 완료",
-          content: reactionData,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        message: "댓글 반응 처리 완료",
+        content: reactionData,
+      },
+    });
   },
 };
