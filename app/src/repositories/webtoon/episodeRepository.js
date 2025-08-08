6920ddbc31499628350fea87716f013d6b7ea1d1
LDH <leedoh0384@gmail.com>
2025-08-08 11:25:00 +0900

    refactor: (#29) episodeRepository.js 쿼리문 통일 및 오름차순 정렬 추가
    - 쿼리문 스타일 통일
    - 오름차순 정렬 추가
    - 응답 형식 camelCase 변환 적용

--- app/src/repositories/webtoon/episodeRepository.js
+++ app/src/repositories/webtoon/episodeRepository.js
@@ -4,36 +4,37 @@ const pool = require("@config/db");
 const toCamelCase = require("@utils/toCamelCase.js");
 
 class EpisodeRepository {
-  // 웹툰에 맞는 회차 불러오기
+  // 웹툰에 맞는 전체 회차 불러오기
   async getEpisodesByWebtoonId(webtoonId) {
     const query = `
         SELECT
-          id,
-          episode_no,
-          title,
-          thumbnail_url,
-          posted_time,
-          rating_avg
-        FROM episodes
-        WHERE webtoon_id = ?;
-        `;
+          ep.id,
+          ep.episode_no,
+          ep.title,
+          ep.thumbnail_url,
+          ep.posted_time,
+          ep.rating_avg
+        FROM episodes ep
+        WHERE ep.webtoon_id = ?
+        ORDER BY ep.episode_no ASC;
+      `;
     const [rows] = await pool.query(query, [webtoonId]);
-    return rows;
+    return toCamelCase(rows);
   }
 
-  // 회차 상세정보 불러오기
+  // 회차클릭 후 만화 불러오기
   async getEpisodeDetailById(episodeId) {
     const query = `
         SELECT
-          e.id AS episode_id,
-          e.episode_no,
-          e.title AS episode_title,
-          e.full_img_url,
-          w.title AS webtoon_title
-        FROM episodes e
-        JOIN webtoons w ON e.webtoon_id = w.id
-        WHERE e.id = ?;
-        `;
+          ep.id AS episode_id,
+          ep.episode_no,
+          ep.title AS episode_title,
+          ep.full_img_url,
+          wt.title AS webtoon_title
+        FROM episodes ep
+        JOIN webtoons wt ON ep.webtoon_id = wt.id
+        WHERE ep.id = ?;
+      `;
     const [rows] = await pool.query(query, [episodeId]);
     return toCamelCase(rows[0]);
   }
