9b9c230518512937601c8c12974b78f6dc8cc436
YJE <c0d1ngyje@gmail.com>
2025-08-23 14:37:51 +0900

    chore: (#54) docker-compose, Dockerfile, .dockerignore 작성 및 루트 컨트롤러 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("Hello Induktoon!");
+});
 
 // 컨트롤러
 const authCtrl = require("@routes/auth/authController");
