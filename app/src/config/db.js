43adeb9bed2ba454fb21ad0e631583bdbd3bca74
YJE <c0d1ngyje@gmail.com>
2025-08-27 22:41:37 +0900

    fix: (#58) DB와 JSON 반환 시간대 불일치 문제 해결

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -7,6 +7,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  timezone: "+00:00",
 });
 
 module.exports = db;
